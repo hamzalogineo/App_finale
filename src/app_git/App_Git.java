1ea0d4bf21bf1fd8173bdfc800a839894572edc8
unknown <hamzadiakit@gmail.com>
2022-01-19 11:52:09 +0000

    19-01-2022 first commit

---
+++ src/app_git/App_Git.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app_git;
+
+import frontend.Connexion;
+
+/**
+ *
+ * @author hp
+ */
+public class App_Git {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Connexion con = new Connexion() ;
+                  con.setVisible(true) ;
+                  
+    }
+    
+}
